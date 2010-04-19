c5a89ee7ea4204f43fa27c3d18b398b42d864ce9
Eric Davis <edavis@littlestreamsoftware.com>
2010-04-19 08:23:10 -0700

    Generate plugin.

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_simple_support do
+  name 'Redmine Simple Support plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
