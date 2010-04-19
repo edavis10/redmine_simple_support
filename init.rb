e3672a2ae14440b3ea36d4175e57b52b8137a161
Eric Davis <edavis@littlestreamsoftware.com>
2010-04-19 08:43:32 -0700

    [#3899] Updated init.rb

--- init.rb
+++ init.rb
@@ -1,8 +1,13 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_simple_support do
-  name 'Redmine Simple Support plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  name 'Redmine Simple Support'
+  author 'Eric Davis'
+  description 'Allows linking Redmine issues to external resources like a third party bug tracker to support system.'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-simple-sup'
+  author_url 'http://www.littlestreamsoftware.com'
+
+  version '0.1.0'
+
+  requires_redmine :version => '0.9.2'
 end
